2d9631e604a100d475a4c765d9a38e76a9b80c1f
Grace Karina <gracekarina12@gmail.com>
2025-10-05 19:56:21 +0700

    feat: enhance project section

--- src/modules/homepage-module/section/project-section.tsx
+++ src/modules/homepage-module/section/project-section.tsx
@@ -1,9 +1,38 @@
-import { ProjectDesktop } from "../components/project/project-desktop";
+import { ProjectSlider } from "../components/project/project-slider";
 
 export const ProjectSection = () => {
   return (
-    <section>
-      <ProjectDesktop />
+    <section className="bg-corvidian-5 relative overflow-visible pb-0">
+      <div className="container mx-auto pt-7">
+        <div className="flex flex-col lg:flex-row justify-between mb-12 px-6 lg:px-20 gap-6 text-center lg:text-left lg:items-end max-lg:text-center">
+          <div className="pt-10">
+            <p className="text-corvidian-1 font-bold mb-2 flex justify-center lg:justify-start items-center gap-2 lg:text-2xl">
+              <span
+                className="w-[14px] h-[14px] rounded-full"
+                style={{
+                  backgroundColor: "var(--Corvidian-2, #1578CB)",
+                  border: "2px solid var(--Corvidian-3, #C5CED5)",
+                }}
+              ></span>
+              Project Kami
+            </p>
+            <h2 className="lg:text-2xl xl:text-4xl sm:text-3xl font-extrabold text-corvidian-1 leading-snug max-md:text-[20px]">
+              Optimalkan Bisnis Anda <br />
+              dengan Solusi IT yang Tepat.
+            </h2>
+          </div>
+
+          <p className="text-corvidian-1 text-sm sm:text-base lg:text-md xl:text-[17px] max-w-[320px] leading-relaxed lg:text-right max-lg:mx-auto">
+            Mengembangkan aplikasi khusus <br className="hidden max-md:block" />{" "}
+            dan layanan cloud untuk bisnis Anda
+          </p>
+        </div>
+      </div>
+
+      <div className="max-w-[1536px] mx-auto">
+        <ProjectSlider />
+      </div>
+
     </section>
   );
-}
+};

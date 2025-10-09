831fadc2ca5ea62763f2231a5fa60b34cc047658
Grace Karina <gracekarina12@gmail.com>
2025-10-09 21:27:11 +0700

    feat: update navbar component to conditionally render desktop and mobile versions

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { NavbarDesktop } from './navbar-desktop'
+import React from "react";
+import { NavbarDesktop } from "./navbar-desktop";
+import { NavbarMobile } from "./navbar-mobile";
 
 export const Navbar = () => {
   return (
     <div>
-      <NavbarDesktop />
+      <div className="hidden lg:block">
+        <NavbarDesktop />
+      </div>
+      <div className="block lg:hidden">
+        <NavbarMobile />
+      </div>
     </div>
-  )
-}
-
-
-
- 
-
-
+  );
+};

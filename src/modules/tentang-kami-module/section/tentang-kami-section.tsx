701348b227918e900638a7f5b7b833aeb35365e6
gracekarinn <gracekarina12@gmail.com>
2025-10-17 14:22:37 +0700

    feat: create tentang kami section component

---
+++ src/modules/tentang-kami-module/section/tentang-kami-section.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header } from '../components/header'
+import { ArtikelKami } from '../components/artikel-kami'
+
+
+const TentangKamiSection = () => {
+  return (
+    <section>
+      <Header />
+      <ArtikelKami />
+    </section>
+  )
+}
+
+export default TentangKamiSection

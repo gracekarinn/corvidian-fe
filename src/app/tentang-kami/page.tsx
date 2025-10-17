0da5c481a27f4c6043cd5e778b91c37e7977608b
gracekarinn <gracekarina12@gmail.com>
2025-10-17 14:18:03 +0700

    feat: create tentang kami page component

---
+++ src/app/tentang-kami/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TentangKamiModule } from '@/modules/tentang-kami-module'
+
+const page = () => {
+  return (
+    <div>
+      <TentangKamiModule />
+    </div>
+  )
+}
+
+export default page

4cf2dd9a91f791b6661352fb982d3894c8f76af9
gracekarinn <gracekarina12@gmail.com>
2025-11-10 13:54:46 +0700

    fix: clean up image section code

--- src/modules/tentang-kami-module/components/image.tsx
+++ src/modules/tentang-kami-module/components/image.tsx
@@ -4,51 +4,71 @@ import Image from 'next/image'
 export const ImageSection = () => {
   return (
     <section className="w-full relative">
-        {/* Desktop Version */}
-        <div className="hidden lg:block">
-          <Image
-            src="/tentangkami/shape.png"
-            alt="Cable Image"
-            width={650}
-            height={481.84}
-            style={{zIndex:3}}
-            className="absolute object-contain top-[0px]"
-          />
-          <div 
-            className="absolute top-[40px] w-full" 
-            style={{
-              backgroundImage: 'url(/tentangkami/hero.png)',
-              zIndex: 1,
-              backgroundSize: 'cover', 
-              backgroundPosition: 'center', 
-              height: '550.84px', 
-              position: 'relative'
-            }}>
-          </div>
-        </div>
+      <div className="hidden lg:block">
+        <Image
+          src="/tentangkami/shape.png"
+          alt="Cable Image"
+          width={650}
+          height={481.84}
+          style={{ zIndex: 3 }}
+          className="absolute object-contain top-0"
+        />
+        <div 
+          className="absolute top-[40px] w-full" 
+          style={{
+            backgroundImage: 'url(/tentangkami/hero.png)',
+            zIndex: 1,
+            backgroundSize: 'cover', 
+            backgroundPosition: 'center', 
+            height: '550.84px', 
+            position: 'relative'
+          }}
+        />
+      </div>
 
-        {/* Mobile Version */}
-        <div className="block lg:hidden">
-          <Image
-            src="/tentangkami/shape.png"
-            alt="Cable Image"
-            width={200}
-            height={90}
-            style={{zIndex:3}}
-            className="absolute object-contain top-[0px] "
-          />
-          <div 
-            className="absolute top-[20px] w-full" 
-            style={{
-              backgroundImage: 'url(/tentangkami/hero.png)',
-              zIndex: 1,
-              backgroundSize: 'cover', 
-              backgroundPosition: 'center', 
-              height: '300px', 
-              position: 'relative'
-            }}>
-          </div>
-        </div>
+      <div className="hidden md:block lg:hidden">
+        <Image
+          src="/tentangkami/shape.png"
+          alt="Cable Image"
+          width={450}
+          height={250}
+          style={{ zIndex: 3 }}
+          className="absolute object-contain top-0"
+        />
+        <div 
+          className="absolute top-[30px] w-full" 
+          style={{
+            backgroundImage: 'url(/tentangkami/hero.png)',
+            zIndex: 1,
+            backgroundSize: 'cover', 
+            backgroundPosition: 'center', 
+            height: '400px', 
+            position: 'relative'
+          }}
+        />
+      </div>
+
+      <div className="block md:hidden">
+        <Image
+          src="/tentangkami/shape.png"
+          alt="Cable Image"
+          width={300}
+          height={90}
+          style={{ zIndex: 3 }}
+          className="absolute object-contain top-0 left-0 right-0"
+        />
+        <div 
+          className="absolute top-[20px] w-full" 
+          style={{
+            backgroundImage: 'url(/tentangkami/hero.png)',
+            zIndex: 1,
+            backgroundSize: 'cover', 
+            backgroundPosition: 'center', 
+            height: '300px', 
+            position: 'relative'
+          }}
+        />
+      </div>
     </section>
   )
 }

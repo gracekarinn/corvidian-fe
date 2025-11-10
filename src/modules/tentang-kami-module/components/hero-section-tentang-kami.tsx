d4ed7bee3ba4af659f59070ace54a398595cb082
gracekarinn <gracekarina12@gmail.com>
2025-11-10 13:54:16 +0700

    fix: improve layout and alignment in hero section

--- src/modules/tentang-kami-module/components/hero-section-tentang-kami.tsx
+++ src/modules/tentang-kami-module/components/hero-section-tentang-kami.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
-// import Image from 'next/image'
 
 const HeroSectionTentangKami = () => {
   return (
     <section>
-       <div className="container relative mt-[40px] mx-auto px-4  pt-12 lg:py-20 z-10 mb-[40px]">
-          <div className="space-y-4 lg:space-y-6 flex flex-row">
-            <div className='w-1/2'>
-                <h1 className="text-xl max-sm:max-w-[330px] md:text-2xl lg:text-4xl  font-extrabold text-corvidian-1 leading-tight max-[350px]:text-2xl max-[350px]:!max-w-[250px]">
+       <div className="container relative mt-[40px] mx-auto px-4 pt-12 lg:py-20 z-10 mb-[40px]">
+          <div className="flex flex-col items-center text-center gap-6 lg:gap-10 md:flex-row md:items-end md:justify-between md:text-left">
+            <div className='w-full md:w-1/2'>
+                <h1 className="text-xl max-sm:max-w-[330px] md:text-2xl lg:text-4xl font-extrabold text-corvidian-1 leading-tight max-[350px]:text-2xl max-[350px]:!max-w-[250px] mx-auto md:mx-0">
                   Mitra Teknologi Cerdas untuk 
                 </h1>
-                <h1 className="text-xl max-sm:max-w-[330px] md:text-2xl lg:text-4xl font-extrabold text-[#2A77BD] leading-tight max-[350px]:text-2xl max-[350px]:!max-w-[250px]">
+                <h1 className="text-xl max-sm:max-w-[330px] md:text-2xl lg:text-4xl font-extrabold text-[#2A77BD] leading-tight max-[350px]:text-2xl max-[350px]:!max-w-[250px] mx-auto md:mx-0">
                   Pertumbuhan Bisnis Lebih Cepat
                 </h1>
             </div>
-            <div className="w-1/2 text-end">
-                <p className='text-[12px] ps-[50%] pt-0 lg:pt-[50px]'>Ubah tantangan kompleks bisnis melalui solusi digital yang relevan, skalabel, dan berdampak nyata.</p>
+            <div className="w-full md:w-1/2 md:text-end">
+                <p className='text-sm xl:text-[16px] 2xl:text-[18px] leading-relaxed md:leading-snug lg:max-w-md md:ml-auto'>
+                  Ubah tantangan kompleks bisnis <br className="hidden sm:block" />
+                  melalui solusi digital yang relevan, <br className="hidden sm:block" />
+                  skalabel, dan berdampak nyata.
+                </p>
             </div>
           </div>
         </div>

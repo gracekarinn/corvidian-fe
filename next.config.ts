955b0e3cffe39d5dd6edb2fb9b85292af5ea212a
gracekarinn <gracekarina12@gmail.com>
2025-11-27 13:02:12 +0700

    fix: update remotePatterns configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -22,12 +22,14 @@ if (apiUrl) {
   remotePatterns.push({
     protocol,
     hostname: apiUrl.hostname,
+    pathname: "/**",
   });
 }
 
 const nextConfig: NextConfig = {
   images: {
-    formats: ['image/avif', 'image/webp'],
+    remotePatterns, 
+    formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
